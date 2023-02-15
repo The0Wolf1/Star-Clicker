b8118ddca8bc2eccb63612cb66ffd62784bdec09
The0Coder <61795411+The0Wolf1@users.noreply.github.com>
2023-02-15 00:06:43 +0000

    Updated save and load functions added

--- index.js
+++ index.js
@@ -20,9 +20,6 @@ var lasers = 0;
 var bombs = 0;
 var nukes = 0;
 
-//Save variables
-var Game = new NewGame();
-
 function increment() {
     stars += starsPerClick;
     document.getElementById("showStars").innerHTML = stars;
@@ -196,25 +193,24 @@ function restart(){
     document.getElementById("nukeCost").innerHTML = nukeCost;
 }
 
-function NewGame() {
-	this.name = "Our Game";
-	this.description = "Our Game's Description";
-	this.numbers = [];
-	for (var i=0;i<10;i++) {
-		this.numbers[i] = i;
-	}
+function save(){
+    var save = {
+        stars: stars,
+        starsPerSecond: starsPerSecond,
+        miners: miners,
+        drills: drills,
+        lasers: lasers,
+        bombs: bombs,
+        nukes: nukes,
+        minersCost: minersCost,
+        drillsCost: drillsCost,
+        laserCost: laserCost,
+        nukeCost: nukeCost
+    }
+    localStorage.setItem("save",JSON.stringify(save));
+    ga('send', 'event', 'My Game', 'Save');
 }
 
-window.onload = function() {
-	window.localStorage['SaveName'] = JSON.stringify(Game);
-	
-	window.GameTwo = JSON.parse(window.localStorage['SaveName']);
-	
-	document.getElementById("name").innerHTML = "Name: " + GameTwo.name;
-	document.getElementById("desc").innerHTML = "Description: " + GameTwo.description;
-	var numbers = "";
-	for (var i=0;i<10;i++) {
-		if (numbers.length == 0) {numbers = i;} else {numbers += ", " + i;}
-	}
-	document.getElementById("numbers").innerHTML = "Numbers: " + numbers;
+function load(){
+    var savegame = JSON.parse(localStorage.getItem("save"));
 }
